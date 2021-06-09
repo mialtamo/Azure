8c6c9e5fddcb4168afc76cc241e6ba6d91dfd418
mialtamo <54998835+mialtamo@users.noreply.github.com>
2021-06-09 15:36:38 -0700

    Update Set AZ Firewall and vnet in all SA's.ps1
    Passive only works for now

--- Set AZ Firewall and vnet in all SA's.ps1
+++ Set AZ Firewall and vnet in all SA's.ps1
@@ -1,40 +1,94 @@
-# Get VNET subnets with storage endpoints
-#Codebase used from MSFT Fabien Gilbert
-#Code locks down to specific vnets within a resource group
-#WILL add vnets subnets to storage accounts even if service endpoints are NOT configured.
+#Connect-AzAccount
+Write-Host "*************************************************************************************************" -ForegroundColor blue
+Write-Host " "
+Write-Host "Welcome to the Azure Storage Account Firewall Activation and Virtual Network Configuration System" -ForegroundColor blue
+Write-Host " "
+Write-Host "To Begin run Start-AzSAFWConfig" -ForegroundColor Blue
+Write-Host " "
+Write-Host "*************************************************************************************************" -ForegroundColor Blue
 
-$rsgs = Get-AzResourceGroup #-name "<NAME OF A SINGLE RESOURCE GROUP CAN BE USED FOR TESTING.>"
+Function Start-AzSAFWConfig {
+    Write-Host "[Start-AzSAFWConfig] Started..." -ForegroundColor Blue
+Do {
+    $passive = Read-Host -Prompt "Should this be a passive Scan Only? ( (P)assive or (E)nforce or (I)nformation )"
+If ($passive -match "I"){
+Write-Host "Information: By selecting passive scanning it will simply report findings and not attempt to make any changes to storage accounts or virtual networks in ANY capacity. If you select Enforce you will being making the necessary changes discovered in passive Mode. " -ForegroundColor Green
+}
+elseIf ($passive -match "E"){
+    Write-Host "This scan will NOT be passive. Ctrl+C to stop this before countdown" -ForegroundColor red
+    $delay = 10
+    while ($delay -ge 0){
+        Write-Host "Seconds Remaining: $($delay)" -ForegroundColor red
+        start-sleep 1
+        $delay -= 1
+    }
+Write-Host "Begin Non-Passive sweep" -ForegroundColor Red
+}
 
+elseIf ($passive -match "P"){
+
+Write-Host "Passive Scan selected, this will simply report findings" -ForegroundColor green
+ #Loop through each resource group
+$rsgs = Get-AzResourceGroup
+$foundSA = 0
+$foundVN = 0
 foreach ($rsg in $rsgs){
+Write-Host "Scanning the current Azure Resource Group named ..... < $($rsg.ResourceGroupName) > for any Storage Accounts where the firewall is set to (ALLOW) and for any Virtual Networks where the Microsoft.Storage Service Endpoint is missing" -ForegroundColor green
+Write-Host ""
+Write-Host "Scanning for Storage Accounts.... That meet our requirements..." -ForegroundColor Blue
+$azureSAs = Get-AzStorageAccount -ResourceGroupName $rsg.ResourceGroupName | where-object {$_.NetworkRuleSet.DefaultAction -eq "Allow"}
+foreach ($azureSA in $azureSAs)
+{
+If ($azureSA){
+Write-Host "Storage Account found named: $($azureSA.StorageAccountName)" -ForegroundColor yellow
+Write-Host "Firewall Status: $($azureSA.NetworkRuleSet.DefaultAction)" -ForegroundColor yellow
+$foundSA++
+}
+else{
+    Write-Host "No Azure Storage Account found that matched our needs within <$($rsg.ResourceGroupName) >" -foregroundcolor green
+} #End If statement for AzureSA
+} #End foreach loop AzureSA
+If($azureSAs){
+    ######################STARTING VNET SEARCH FOR WHEN A RESOURCE GROUP CONTAINS A STORAGE ACCOUNT ##################################
+Write-Host "Scanning for Virtual Networks.... That meet our requirements..." -ForegroundColor Blue
 $vnets = Get-AzVirtualNetwork -ResourceGroupName $rsg.ResourceGroupName
-$vnetSubRules = @()
-foreach ($vnet in $vnets) { 
-    $snwse = 0
-    foreach ($vnetSub in $vnet.Subnets) {
-        
-            $vnetSubRules += @{
-                "VirtualNetworkResourceId" = $vnetSub.Id
-                "Action" = "allow"
-            }
-            $snwse++
-            
-        }
-     
-    
-    }   
-    Write-Output ("found " + $snwse + " subnets with storage account service endpoint in VNET " + [char]34 + $vnet.Name + [char]34 + "...")
-# Close the loop if needed }
-# Get Storage Accounts
-$storaccts = Get-AzStorageAccount -ResourceGroupName $rsg.ResourceGroupName
-# Loop through storage accounts
-foreach($storacct in $storaccts){
-    Write-Output ("configuring storage account firewall on " + [char]34 + $storacct.StorageAccountName + [char]34 + " current default action: " + $storacct.NetworkRuleSet.DefaultAction)
-    $updateNetworkRules = Update-AzStorageAccountNetworkRuleSet -ResourceGroupName $storacct.ResourceGroupName `
-                                                                -AccountName $storacct.StorageAccountName `
-                                                                -Bypass AzureServices,Logging,Metrics `
-                                                                -DefaultAction Deny `
-                                                                -IpRule $publicIpRules `
-                                                                -VirtualNetworkRule $vnetSubRules `
-                                                                -WarningAction:SilentlyContinue
+foreach ($vnet in $vnets) {
+If ($vnet.Subnets.ServiceEndpoints.Service -notcontains "Microsoft.Storage") {
+    Write-Host "This virtual network does NOT have the service endpoint listed as REQUIRED" $vnet.name -ForegroundColor yellow
+    Write-Host "Here are the Service Endpoints discovered for this vnet's subnets:" -ForegroundColor blue
+    $vnet.Subnets.ServiceEndpoints.Service
+    $foundVN++
+} #End if statement vnet
+else {
+    Write-Host "No Azure Virtual Networks found that matched our needs within <$($rsg.ResourceGroupName) > where storage account < $($azureSA.StorageAccountName)> " -foregroundcolor green
 }
+} #End foreach loop vnet
+######################ENDING VNET SEARCH FOR WHEN A RESOURCE GROUP CONTAINS A STORAGE ACCOUNT ##################################
 }
+else {
+    Write-Host "No Storage Account was discovered" -ForegroundColor red
+}
+}#End RSG Foreach Loop
+Write-Host "Passive Scanning Complete...." -ForegroundColor Blue
+Write-Host "Building Report....Standby" -ForegroundColor Blue
+start-sleep 2
+Write-Host "*************************************************************************************************" -ForegroundColor Blue
+Write-Host ""
+Write-Host "                                           REPORT FINDINGS" -ForegroundColor Blue
+Write-Host "-------------------------------------------------------------------------------------------------" -Foregroundcolor Blue
+Write-Host "                        Total Number of Storage Accounts found that need correction: $($foundSA) " -ForegroundColor Blue
+Write-Host "" 
+Write-Host "                        Total Number of Virtual Networks found that need correction: $($foundVN) " -ForegroundColor Blue
+Write-Host "" 
+Write-Host ""
+Write-Host "*************************************************************************************************" -ForegroundColor Blue
+
+} #End $passive Yes
+
+
+    else{
+  Write-Host  "Incorrect Option Specified" -ForegroundColor red
+}
+} Until ($passive -match "y" -or $passive -match "n") #End Do Loop
+
+}#end Function
